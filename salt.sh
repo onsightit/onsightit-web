e09c609fe75c5ad934c8b62efc60aa74ef3cfbfa
OnsightIT <onsightit@gmail.co>
2017-11-15 07:51:25 -0700

    .

--- salt.sh
+++ salt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "*** Starting salt-project..."
-MYSQLUSER=salty MYSQLPASS=P4ssW0rd NODE_ENV=development nodemon src/server/server.js
-#MYSQLUSER=salty MYSQLPASS=P4ssW0rd NODE_ENV=production node src/server/server.js
-#MYSQLUSER=salty MYSQLPASS=P4ssW0rd NODE_ENV=test jest --runInBand
+MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=development nodemon src/server/server.js
+#MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=production node src/server/server.js
+#MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=test jest --runInBand
