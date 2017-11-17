f1be1fd0a74a98688e3a2ca165dd0c12b0ac236b
Steve Woods <sdwoods@svr1.onsightit.com>
2017-11-17 07:36:39 -0700

    start

--- salt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "*** Starting salt-project..."
-MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=development nodemon src/server/server.js
-#MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=production node src/server/server.js
-#MYSQLUSER=salty MYSQLPASS=XXXXXXXX NODE_ENV=test jest --runInBand
